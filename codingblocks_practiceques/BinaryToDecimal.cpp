0de7db369828b58863c3c420c75f0a6a89fcbb3c
Sanya Singla <78159088+sanya-3-chitkara@users.noreply.github.com>
2022-07-22 18:31:37 +0530

    Create BinaryToDecimal.cpp

---
+++ codingblocks_practiceques/BinaryToDecimal.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int num;
+	cin>>num;
+	int temp=num;
+	int base=1;
+	int dec_value=0;
+	while(temp){
+		int lastdigit=temp%10;
+		temp=temp/10;
+		dec_value+=lastdigit*base;
+		base*=2;
+	}
+	cout<<dec_value<<endl;
+	
+	return 0;
+}
