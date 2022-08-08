daf64a4e71e33493f7f72df9cf68149b702b4c40
Sanya Singla <78159088+sanya-3-chitkara@users.noreply.github.com>
2022-08-08 16:16:27 +0530

    Create String-StringCompression.cpp

---
+++ codingblocks_practiceques/String-StringCompression.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str;
+    cin>>str;
+    int n=str.length();
+    for(int i=0;i<n;i++){
+        int count=1;
+        while(i<n-1 && str[i]==str[i+1]){
+            count++;
+            i++;
+        }
+		if(count==1){
+			cout<<str[i];
+		}
+		else{
+			cout<<str[i]<<count;
+		}
+            
+    }
+    
+   
+
+    return 0;
+}
